d2273b23454fda5c88015ac6196ee7cb742a75c2
Yasmin101-101 <yasminmohamudyasmin@gmail.com>
2026-01-21 20:36:19 +0300

    deleted some comment

--- clock-color/src/main.jsx
+++ clock-color/src/main.jsx
@@ -1,13 +1,4 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import App from './App' 
-// import './App.css' // global styles 
 
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-// )
 
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
